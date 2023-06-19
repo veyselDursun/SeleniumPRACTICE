ed5009e3079ec6ea0781f9a2d84150b503697127
veyselDursun <veysdursun@yahoo.com>
2023-06-19 08:36:37 +0200

    P01Tekrar

---
+++ src/test/java/Day1_Tekrar/P01.java
@@ -0,0 +1,40 @@
+package Day1_Tekrar;
+
+public class P01 {
+                    //....Exercise-1:...
+    //Create a new class with main method
+    public static void main(String[] args) throws InterruptedException {
+
+        //Set Path
+
+
+        //Create a chrome driver
+
+
+        //Maximize window
+
+
+        //Open google home page https://www.google.com
+
+
+        //On the same class, navigate to amazon home page https://www.amazon.com and navigate back to google
+
+
+        //Wait about 4 sn
+
+
+        //Navigate forward to amazon
+
+
+        //Refresh page
+
+
+        //Close/quit the browser
+
+
+        //And Last step print "All is well" on console
+
+
+    }
+}
+
