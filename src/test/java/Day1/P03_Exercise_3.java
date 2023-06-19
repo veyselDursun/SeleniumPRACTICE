ba6fb041b16093f4d32c236eb3800199069f16ea
veyselDursun <veysdursun@yahoo.com>
2023-06-19 08:14:41 +0200

    P04

--- src/test/java/Day1/P03_Exercise_3.java
+++ src/test/java/Day1/P03_Exercise_3.java
@@ -1,5 +1,6 @@
 package Day1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,19 +15,26 @@ public class P03_Exercise_3 {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
    //Navigate to  https://testpages.herokuapp.com/styled/index.html
-        driver.navigate().to("https://testpages.herokuapp.com/styled/index.html");
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
 
    //Click on  Calculater under Micro Apps
+        driver.findElement(By.id("calculatetest"));
 
    //Type any number in the first input
+        driver.findElement(By.id("number1")).sendKeys("12");
 
    //Type any number in the second input
+        driver.findElement(By.id("number2")).sendKeys("8");
 
    //Click on Calculate
+        driver.findElement(By.id("calculate")).click();
 
    //Get the result
+        String result = driver.findElement(By.xpath("//*[@id='answer']")).getText();
 
    //Print the result
+        System.out.println("RESULT = " + result);
+        driver.close();
 
     }
 }
---
+++ src/test/java/Day1/P04_Exercise_4.java
@@ -0,0 +1,56 @@
+package Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class P04_Exercise_4 {
+    public static void main(String[] args) {
+
+        System.setProperty("Webdriver.chrome.driver","resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //Navigate to website https://testpages.herokuapp.com/styled/index.html
+        driver.navigate().to("https://testpages.herokuapp.com/styled/index.html");
+
+        //Under the ORIGINAL CONTENTS click on Alerts
+        driver.findElement(By.id("alerts")).click();
+
+        //print the URL
+        System.out.println(driver.getCurrentUrl());
+
+        //navigate back
+        driver.navigate().back();
+
+        //print the URL
+        System.out.println(driver.getCurrentUrl());
+
+        //Click on Basic Ajax
+        driver.findElement(By.xpath("//*[@id='basicajax']")).click();
+
+        //print the URL
+        System.out.println(driver.getCurrentUrl());
+
+        //enter value 20 and ENTER
+        driver.findElement(By.id("lteq30")).sendKeys("20"+ Keys.ENTER);
+
+        //and then verify Submitted Values is displayed open page
+        String expectedResult="Submitted Values";
+        String actualResult=driver.findElement(By.xpath("//*[text()='Submitted Values']")).getText();
+        if (expectedResult.equals(actualResult)){
+            System.out.println("Test Başarılı");
+        }else {
+            System.out.println("Test Başarısız");
+        }
+
+        //close driver
+
+        driver.close();
+
+    }
+}
