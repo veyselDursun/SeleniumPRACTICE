9989fb3fb0eaca8a1bdd354c4b90ef8909973ce9
veyselDursun <veysdursun@yahoo.com>
2023-06-23 12:49:33 +0200

    day02

---
+++ src/test/java/tests/day02/P03.java
@@ -0,0 +1,26 @@
+package tests.day02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.TestBase;
+
+public class P03 extends TestBase {
+
+    //BeforeClass ile driver'i oluşturun ve class icinde static yapin
+    //Maximize edin ve 10 sn bekletin
+    //http://www.qooqle.com adresine gidin
+    //1-arama kutusuna "12 Angry Men" yaz ip, cikan sonuc şayisini yazdinin
+    //2-arama kutusuna "Vizontele" yaz ip, cikan sonuc şayisini yazdirin
+    //3-arama kutusuna "Saving Private Ryan" yaz4p, cikan sonuc şayisini yazdirin
+    //AfterClass ile kapatin.
+
+    static WebDriver driver;
+
+
+    @BeforeClass
+
+    }
+}
